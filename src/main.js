26ce155c61f4b3eb989e2a82b50b9ab390daf456
Max Anderson MOUMOUNI <happydev.229@gmail.com>
2023-02-20 22:12:03 +0100

    ajout de la vue todo

--- src/main.js
+++ src/main.js
@@ -1,43 +1,41 @@
-import { createApp } from 'vue/dist/vue.esm-bundler.js'
-
+import { createApp } from "vue/dist/vue.esm-bundler.js";
+import Todo from "../todo.vue";
 // import './assets/main.css'
 
 const app = createApp({
-    data() {
-        return {
-          title: "Todo App",
-          text: "",
-          todos: [
-            
-          ],
-        };
-    }, 
-    methods: {
-        addTodo() {
-            this.todos.push({
-              id: Date.now(),
-              title: this.text,
-              date: date_to_str(),
-              class: "",
-            });
-            this.text = ''
-        }, 
-        removeTodo(id) {
-            this.todos.filter((todo) => todo.id == id)[0].class = "fadeout"
-            setTimeout(() => {
-                this.todos = this.todos.filter(todo => todo.id !== id)
-            }, 200)
-            // this.todos = this.todos.filter(todo => todo.id !== id)
-        }
-    }
-})
+  data() {
+    return {
+      title: "Todo App",
+      text: "",
+      todos: [],
+    };
+  },
+  components: {
+    Todo,
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: Date.now(),
+        title: this.text,
+        date: date_to_str(),
+        class: "",
+      });
+      this.text = "";
+    },
+    removeTodo(id) {
+      this.todos.filter((todo) => todo.id == id)[0].class = "fadeout";
+      setTimeout(() => {
+        this.todos = this.todos.filter((todo) => todo.id !== id);
+      }, 200);
+      // this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+  },
+});
 
+app.mount("#app");
 
-app.mount('#app')
-
-
-function date_to_str(date=Date.now()) {
-    let d = new Date(date)
-    return d.toLocaleString()
-
+function date_to_str(date = Date.now()) {
+  let d = new Date(date);
+  return d.toLocaleString();
 }
---
+++ todo.vue
@@ -0,0 +1,16 @@
+<template>
+  <li :key="todo.id" class="todo" :class="todo.class">
+    <div class="left">
+      <div class="name">{{ todo.title }}</div>
+      <div class="date">{{ todo.date }}</div>
+    </div>
+    <button @click="remove(todo.id)">Supprimer</button>
+  </li>
+</template> 
+
+<script >
+export default {
+  name: 'Todo',
+  props: { todo: Object, remove: Function, },
+}
+</script>   
